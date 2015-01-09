9830b4a01cd5c359f78616f9a1990bfc35fb7b4d
James Maynard <jameskirkmaynard@gmail.com>
2015-01-09 15:35:21 -0800

    Straightens the code on method up a little

--- lib/find_word.rb
+++ lib/find_word.rb
@@ -3,20 +3,22 @@ class String
     upcased_search_word = search_word.upcase()
     totals_array = []
     superfluous = "You use that word waay too much"
+
     user_sentence = self.gsub(/[^a-z0-9\s]/i, '')
     substitute_sentence = user_sentence.upcase().split()
-      substitute_sentence.each() do |word|
+
+    substitute_sentence.each() do |word|
         if word == upcased_search_word
           totals_array.push(upcased_search_word)
         end
       end
     totals_array.length()
 
-      if totals_array.length().>=(8)
-          superfluous
-      else
-          totals_array.length()
-      end
+    if totals_array.length().>=(8)
+        superfluous
+    else
+        totals_array.length()
+    end
 
   end
 end
