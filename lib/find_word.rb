37b916beefe6666ca3259a1dc889fd3ec4831b67
James Maynard <jameskirkmaynard@gmail.com>
2015-01-09 08:43:44 -0800

    Wrote outline code for specs & method

---
+++ lib/find_word.rb
@@ -0,0 +1,5 @@
+class String
+  define_method(:findword) do
+  #code will go here  
+  end
+end
---
+++ spec/fine_word_spec.rb
@@ -0,0 +1,7 @@
+define('rspec')
+define('findword')
+
+describe('String#findword') do
+  #eventually put code here
+
+end
