fbe88d0f4e77e61fe6a961fc554aaaf9b5ad16b0
James Maynard <jameskirkmaynard@gmail.com>
2015-01-09 11:40:13 -0800

    Writes layout into .erb files. Ran and passes web application

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require('sinatra')
 require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/find_word.rb')
 
 get('/') do
   erb(:word_to_find)
 end
 
 get('/number_of_times') do
-
+  @sentence = params.fetch('text')
+  @word = params.fetch('word')
+  @number_of_times = @sentence.findword(@word)
   erb(:number_of_times)
 end
