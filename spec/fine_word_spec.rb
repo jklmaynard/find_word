199754081b5f59ace8b3f9bfa1ca4a4838c9881a
James Maynard <jameskirkmaynard@gmail.com>
2015-01-09 09:41:09 -0800

    Write first spec

--- spec/fine_word_spec.rb
+++ spec/fine_word_spec.rb
@@ -1,7 +1,8 @@
-define('rspec')
-define('findword')
+require('rspec')
+require('find_word')
 
 describe('String#findword') do
-  #eventually put code here
-
+  it("will find the word 'one' once and return the fix number 1") do
+  expect(("word one").findword("one")).to(eq(1))  
+  end
 end
